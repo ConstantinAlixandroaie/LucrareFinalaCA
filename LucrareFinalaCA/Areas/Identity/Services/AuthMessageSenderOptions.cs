59614fb1afd051a94b13510032c95da08613dbb7
Alixandroaie Constantin <constantin.alixandroaie@gmail.com>
2020-02-11 11:46:10 +0200

    65132

--- LucrareFinalaCA/Areas/Identity/Services/AuthMessageSenderOptions.cs
+++ LucrareFinalaCA/Areas/Identity/Services/AuthMessageSenderOptions.cs
@@ -7,7 +7,7 @@ namespace LucrareFinalaCA.Services
 {
     public class AuthMessageSenderOptions
     {
-        public string SendGridUser;
-        public string SendGridKey;
+        public string SendGridUser= "constantin.alixandroaie";
+        public string SendGridKey= "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";
     }
 }
